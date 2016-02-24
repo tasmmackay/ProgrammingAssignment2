1040fd726ad11d1f8a6bb295378dae519e6517ac
TanyaM <tasm.mackay@icloud.com>
2016-02-23 21:00:54 -0800

    my attempt to solve
    I hope this will work for my quiz

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two function could set an original matrix and calculate its inverse one and store it
 
-## Write a short comment describing this function
+## Using "set()" to set the original matrix and "get()" to get this matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<<-NULL
+    set<-function(y=matrix()) 
+        x<<-y
+    get<-function() 
+        x
+    list(set=set,get=get)
 }
 
 
-## Write a short comment describing this function
+## Calculating the inverse of the matrix and store it. Using "if" to judge whether this matrix have been calculated before.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+Solve <- function(x, ...) {
+    if (!is.null(inv)){
+        return(inv)
+    }
+    inv<<-solve(x$get())
+    inv## Return a matrix that is the inverse of 'x'
 }
